4270f22ecc4e6a005be16c089ab3bfe80158ba00
Michel Maas <michel@maas-fam.nl>
2023-09-29 23:53:53 +0200

    Nieuwe Browser management

--- filerenamer-frontend/src/main/kotlin/nl/maas/filerenamer/frontend/BrowserManager.kt
+++ filerenamer-frontend/src/main/kotlin/nl/maas/filerenamer/frontend/BrowserManager.kt
@@ -1,6 +1,7 @@
 package nl.maas.filerenamer.frontend
 
 import nl.maas.filerenamer.io.FileUtils
+import nl.maas.wicket.framework.viewer.Viewer
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebDriverException
 import org.openqa.selenium.edge.EdgeDriver
@@ -29,134 +30,14 @@ class BrowserManager private constructor() : ApplicationListener<ApplicationRead
 
     @Inject
     private lateinit var appContext: ApplicationContext
-    lateinit var driver: WebDriver
-    val os =
-        if (System.getProperty("os.name").lowercase(Locale.getDefault()).contains("win")) "win" else System.getProperty(
-            "os.name"
-        ).lowercase(Locale.getDefault())
-    val arch = if (System.getProperty("os.arch").lowercase(Locale.getDefault()).contains("64")) "64" else "32"
-    val pathDelimiter = if (os.equals("win")) "\\" else "/"
+    val viewer = Viewer.get()
     private fun startBrowser() {
         val url = "http://localhost:8080"
-        try {
-            if (os.indexOf("win") >= 0) {
-                startWinBrowser(url)
-            } else if (os.indexOf("mac") >= 0) {
-                startMacBrowser("url")
-            } else if (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0) {
-                startNixBrowser(url)
-            }
-        } catch (ex: IOException) {
-            throw RuntimeException(ex)
-        }
+        viewer.startBrowser(url)
     }
 
     fun close() {
-        driver.close()
-    }
-
-    @Throws(IOException::class)
-    private fun startNixBrowser(url: String) {
-        System.setProperty(
-            "webdriver.edge.driver",
-            "${findWebDrivers(Paths.get("${pathDelimiter}linux${pathDelimiter}msedgedriver"))}"
-        )
-        startWebDriver(url)
-    }
-
-    private fun updateDriver(version: String) {
-        val url = URL("https://msedgedriver.azureedge.net/$version/edgedriver_${os}${arch}.zip")
-        val path = Path.of("edgedriver_${os}_${version}.zip")
-        try {
-            downloadDriver(url, path)
-            unpackDriver(path)
-        } catch (ex: Exception) {
-            ex.printStackTrace()
-            checkStatus(driver)
-        } finally {
-            cleanUpDriverUpdate(path)
-            startBrowser()
-        }
-    }
-
-    private fun cleanUpDriverUpdate(path: Path) {
-        path.toFile().delete()
-        Paths.get("${System.getProperty("webdriver.edge.driver")}").toFile().setExecutable(true)
-    }
-
-    private fun unpackDriver(path: Path) {
-        Zip.unzip(
-            FileInputStream(path.toFile()),
-            Paths.get("${System.getProperty("webdriver.edge.driver").substringBeforeLast(pathDelimiter)}").toFile()
-        )
-    }
-
-    private fun downloadDriver(url: URL, path: Path?) {
-        url.openStream().use { Files.copy(it, path) }
-        val webdriverLocation = Paths.get("${System.getProperty("webdriver.edge.driver")}").toRealPath().toString()
-            .replaceAfterLast("/", "").replaceAfterLast(pathDelimiter, "")
-        Paths.get(webdriverLocation).toFile().listFiles().forEach { it.delete() }
-    }
-
-
-    private fun findWebDrivers(path: Path): String {
-        return FileUtils.findFile(path.toString())
-    }
-
-    @Throws(IOException::class)
-    private fun startMacBrowser(url: String) {
-        System.setProperty(
-            "webdriver.edge.driver",
-            "${findWebDrivers(Paths.get("${pathDelimiter}mac${pathDelimiter}msedgedriver"))}"
-        )
-        startWebDriver(url)
-    }
-
-    @Throws(IOException::class)
-    private fun startWinBrowser(url: String) {
-        System.setProperty(
-            "webdriver.edge.driver",
-            "${findWebDrivers(Paths.get("${pathDelimiter}windows${pathDelimiter}msedgedriver.exe"))}"
-        )
-        startWebDriver(url)
-    }
-
-    private fun startWebDriver(url: String) {
-        val options = EdgeOptions()
-        options.addArguments("--app=$url")
-            .setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"))
-            .addArguments("--remote-allow-origins=*")
-            .addArguments("-inprivate")
-
-        try {
-            driver = EdgeDriver(options)
-            driver[url]
-            driver.manage().window().maximize()
-            checkStatus(driver)
-        } catch (ex: Exception) {
-            val driverVersion = ex.message?.split("\n")?.filter { it.contains("MSEdge version") }?.firstOrNull()
-                ?.substringAfterLast("version ") ?: "0"
-            val browserVersion =
-                ex.message?.split("\n")?.filter { it.contains("Current browser version") }?.firstOrNull()
-                    ?.substringAfterLast("version is ")?.substringBefore(" with") ?: "0"
-            if (!browserVersion.contains(driverVersion)) {
-                updateDriver(browserVersion)
-            }
-        }
-    }
-
-    private fun checkStatus(driver: WebDriver) {
-        try {
-            while (driver.manage().window().size.height > 0) {
-                Thread.sleep(1000)
-            }
-        } catch (ex: WebDriverException) {
-            val exit = SpringApplication.exit(appContext, ExitCodeGenerator { 0 })
-            driver.quit()
-            System.exit(exit)
-        } catch (e: InterruptedException) {
-            e.printStackTrace()
-        }
+        viewer.close()
     }
 
     override fun onApplicationEvent(event: ApplicationReadyEvent) {
